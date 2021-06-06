93f1835779b27cb2cf784aa1ca96ac52f79f0cb3
minamahmood83 <minamahmood83@gmail.com>
2021-06-06 15:07:56 -0700

    create controlleers folder and thoughts controller.js file

---
+++ controllers/thoughts-controllers.js
@@ -0,0 +1,20 @@
+const { Thoughts, User } = require('../models');
+
+thoughtsControllers = {
+        // get all thoughts
+        getThoughts(req, res) {
+            Thoughts.find({})
+                .populate({
+                    path: 'reactions',
+                    select: '-__v'
+                })
+                .select('-__v')
+                .then(dbThoughtsData => {
+                    res.json(dbThoughtsData);
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.status(400).json(err);
+
+                });
+        }
