16477ff0e50cd4a4cf8ce99ea5ec2fcd7eaa2fdc
minamahmood83 <minamahmood83@gmail.com>
2021-06-06 19:42:50 -0700

    create user has done

--- models/user.js
+++ models/user.js
@@ -1,29 +1,41 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const UserSchema = new Schema({
-            username: {
-                type: String,
-                required: true,
-                trim: true,
-                unique: true
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+            validator: function(v) {
+                return /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(v);
             },
-            email: {
-                type: String,
-                required: true,
-                unique: true,
-                validate: {
-                    validator: function(v) {
-                        return /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(v);
-                    },
-                    message: props => `${props.value} is not a valid email!`
-                }
-            },
-            thoughts: [{
-                type: Schema.Types.ObjectId,
-                ref: 'Thoughts'
-            }],
-            friends: [{
-                type: Schema.Types.ObjectId,
-                ref: 'User'
-            }]
+            message: (props) => `${props.value} is not a valid email!`,
         },
+    },
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: "Thoughts",
+    }, ],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    }, ],
+}, {
+    toJSON: {
+        virtuals: true,
+    },
+});
+
+UserSchema.virtual("friendCount").get(function() {
+    return this.friends.length;
+});
+
+const User = model("User", UserSchema);
+
+module.exports = User;
