bf1958c8f37b565837bfd89394efc7c64c6a897d
minamahmood83 <minamahmood83@gmail.com>
2021-06-06 19:21:58 -0700

    reaction schema

---
+++ models/thought.js
@@ -0,0 +1,27 @@
+const { Schema, model, Types } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const ReactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxLength: 280,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+}, {
+    toJSON: {
+        getters: true,
+    },
+});
