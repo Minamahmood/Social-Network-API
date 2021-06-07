49e3cf21ca3a105e0ddcd3c3d642896a7a688f92
minamahmood83 <minamahmood83@gmail.com>
2021-06-06 19:12:23 -0700

    create user

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -38,3 +38,13 @@ const userController = {
                     res.status(400).json(err);
                 });
         },
+        createUser({ body }, res) {
+            User.create(body)
+                .then(dbUserData => {
+                    res.json(dbUserData);
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.status(400).json(err);
+                });
+        },
