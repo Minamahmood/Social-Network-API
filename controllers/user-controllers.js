4f42d13de4881e5984a22106fab96a6ed446f69e
minamahmood83 <minamahmood83@gmail.com>
2021-06-06 19:14:47 -0700

    add friend

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -87,3 +87,17 @@ const userController = {
                     res.status(400).json(err);
                 });
         },
+        addFriend({ params }, res) {
+            User.findOneAndUpdate({ _id: params.userId }, { $push: { friends: params.friendId } }, { new: true })
+                .then(dbUserData => {
+                    if (!dbUserData) {
+                        res.status(404).json({ message: 'No user found at this id!' });
+                        return;
+                    }
+                    res.json(dbUserData)
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.status(400).json(err);
+                });
+        },
