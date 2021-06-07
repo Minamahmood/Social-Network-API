6c516004f562da13bb2d96bef87a249a791fc9c6
minamahmood83 <minamahmood83@gmail.com>
2021-06-06 19:11:33 -0700

    get user by id

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -19,3 +19,22 @@ const userController = {
                     res.status(400).json(err);
                 });
         },
+        getUserById({ params }, res) {
+            User.findOne({ _id: params.id })
+                .populate({
+                    path: 'thoughts',
+                    select: '-__v'
+                })
+                .select('-__v')
+                .then(dbUserData => {
+                    if (!dbUserData) {
+                        res.status(404).json({ message: 'No user found at this id' });
+                        return
+                    }
+                    res.json(dbUserData)
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.status(400).json(err);
+                });
+        },
