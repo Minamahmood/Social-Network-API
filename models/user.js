b5c74f2ea27d5666344b0c1615aef369e175a087
minamahmood83 <minamahmood83@gmail.com>
2021-06-06 19:25:32 -0700

    create username

--- models/user.js
+++ models/user.js
@@ -1,2 +1,9 @@
+const { Schema, model } = require('mongoose');
 
-
+const UserSchema = new Schema({
+            username: {
+                type: String,
+                required: true,
+                trim: true,
+                unique: true
+            },
